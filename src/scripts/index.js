ae1b793aa3f0b656468f62f5b1abb24b8752fcfd
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-03-02 23:16:15 +0600

    A little bit update

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,12 +12,25 @@ import '../styles/index.scss';
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
-fetch(`${BASE_URL}/todos/1`)
+fetch(`${BASE_URL}/users`)
     .then((res) => {
         return res.json();
     })
     .then((res_data) => {
-        console.log(res_data);
+        // res_data.map(data => console.log(data.name));
+        let x = res_data.map((data) => {
+            let li = document.createElement('li');
+            let text = `Name: ${data.name};   Phone: ${data.phone}`;
+            let textNode = document.createTextNode(text);
+
+            li.appendChild(textNode);
+
+            return li;
+        });
+        x.forEach(element => {
+            document.getElementById('ulId').appendChild(element);
+        });
+        
     })
     .catch((err) => {
         console.log(err);
